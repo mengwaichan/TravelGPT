80d27fbea100a99853d37e2ecb8c46734c034742
Mengwai Chan <chanmengwai1@gmail.com>
2023-12-11 15:26:16 -0500

    fixed firebase error

--- backend/firebase_write.py
+++ backend/firebase_write.py
@@ -1,7 +1,7 @@
 import firebase_admin
 from firebase_admin import credentials, firestore
 
-cred = credentials.Certificate('./firebase_auth/json')
+cred = credentials.Certificate('./firebase_auth.json')
 firebase_admin.initialize_app(cred)
 db = firestore.client()
 
