d0ad8386708d4105ddce87c6ffb8a4ff61b5f16a
Mengwai Chan <chanmengwai1@gmail.com>
2023-12-12 22:19:50 -0500

    added CORS

--- backend/app.py
+++ backend/app.py
@@ -1,12 +1,14 @@
 from flask import Flask
 import firebase_admin
 from firebase_admin import credentials
+from flask_cors import CORS
 from route_blueprint import route_blueprint
 from openai_blueprint import itinerary_blueprint
 from geocoding_blueprint import geocoding_blueprint
 from user_profile_blueprint import profile_blueprint
 
 app = Flask(__name__)
+CORS(app)
 
 cred = credentials.Certificate('./firebase_auth.json')
 firebase_admin.initialize_app(cred)
