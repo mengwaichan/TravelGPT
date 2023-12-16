9ec3ab05efc586cd12149406903239ff1f4b9a1f
latiffetahaj <latiffetahaj@gmail.com>
2023-12-16 00:15:06 -0500

    edited the nav bar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { useUserAuth } from "./UserAuth";
-import logo from "../assets/travel.png";
+import logo from "../assets/travel-logo.png";
 import menu from "../assets/menu.png";
 
 const Navbar = () => {
@@ -24,29 +24,16 @@ const Navbar = () => {
     setIsMenuOpen(false);
   };
 
-  {/* <div>
-        <Link to="/about">
-          <img class="h-auto max-w-xs" src={logo} alt="Logo" />
-          <span>TravelGPT</span>
-        </Link>
-
-        </div> */}
   return (
-    <div dir="rtl" className="relative h-20">
-      <nav className="absolute top-0 inset-x-0 bg-white border-gray-200 dark:bg-gray-900">
-        <div className="table-row-group">
-          <div className="table-row">
-          <div className="table-cell">
-            <button
-              type="button"
-              onClick={() => setIsMenuOpen(!isMenuOpen)}
-            >
-              <img className="h-auto max-w-xs" src={menu} alt="Menu" />
-            </button>
-          </div>
-          <div className={`${isMenuOpen ? "" : "hidden"} table-cell right-2`}>
-            {/* md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700"*/}
-            <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-5 rtl:space-x-reverse">
+      <nav class="bg-gray-200">
+        <div class="flex flex-wrap justify-between items-center px-3 mx-auto">
+
+          <a href="/about" class="flex items-center space-x-4 py-3 px-3">
+          <img class="h-8 w-8" src={logo} alt="Logo" />
+          <span>TravelGPT</span>
+        </a>
+          <div class={`${isMenuOpen ? "" : "hidden  w-full md:block md:w-auto"}`} id="navbar-default">
+            <ul class="flex items-center space-x-3 py-3 px-3 font-bold">
               {user ? (
                 <>
                   <li>
@@ -76,7 +63,7 @@ const Navbar = () => {
               ) : (
                 <>
                   <li>
-                    <Link to="/contact" onClick={closeMenu}>
+                    <Link to="/contact" onClick={closeMenu} class="p-3">
                       Contact
                     </Link>
                   </li>
@@ -94,13 +81,22 @@ const Navbar = () => {
               )}
             </ul>
             </div>
-          </div>
+
+            <button
+              class="md:hidden flex items-center"
+              data-toggle="collapse"
+              data-target="navbar-default"
+              type="button"
+              onClick={() => setIsMenuOpen(!isMenuOpen)}
+            >
+              <img class="h-8 w-8" src={menu} alt="Menu" />
+            </button>
 
 
         </div>
 
       </nav >
-    </div >
+
   );
 };
 
