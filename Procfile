9eeac47e8bef8a9c3b8d4b3a5cc3490f452a077b
Mengwai Chan <chanmengwai1@gmail.com>
2023-12-19 16:45:07 -0500

    deploy

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn -w 4 -b "0.0.0.0:$PORT" backend.app:app
+web: gunicorn -w 4 -b "0.0.0.0:$PORT" --chdir ./backend/ app:app
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   //content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  content: ["./frontend/src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {},
   },
