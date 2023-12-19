f7378b0ba5850b8e5242d8f32d6d07c5658f5533
syedajannath <uhanihon@gmail.com>
2023-12-18 21:01:03 -0500

    change

--- frontend/src/pages/AboutUs.jsx
+++ frontend/src/pages/AboutUs.jsx
@@ -8,13 +8,13 @@ const AboutUs = () => {
 
    
     return (
-        <div className="about-container" max-width= "800px" margin='0 auto' padding= '20px'>
-            <header text-align="center" >
-                <img src={logo} alt="Travel Logo" max-width="100px" />
+        <div className="about-container">
+            <header >
+                <img src={logo} alt="Travel Logo"/>
                 <h1>About Us </h1>
             </header>
-            <section margin-top="20px">
-                <p line-height="1.5">
+            <section>
+                <p>
                     Welcome to our travel community! We are passionate about exploring the world and sharing our experiences with you.
                     whether you are a seasoned traveler or planning your first adventure, our platform is here to 
                     inspire and assist you on your journey.
