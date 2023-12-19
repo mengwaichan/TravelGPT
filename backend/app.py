3f21a8b4d64e36f4fcd5e78003ecd0a351c19f1f
Mengwai Chan <chanmengwai1@gmail.com>
2023-12-19 17:07:20 -0500

    deploy

--- backend/app.py
+++ backend/app.py
@@ -3,6 +3,7 @@ import firebase_admin
 from firebase_admin import credentials
 from flask_cors import CORS
 import os
+from dotenv import load_dotenv
 
 from controllers.route import route_blueprint
 from controllers.itinerary import itinerary_blueprint
@@ -12,6 +13,7 @@ from controllers.place import place_image_blueprint
 
 app = Flask(__name__, static_folder="../frontend/dist", template_folder="../frontend/dist")
 CORS(app) 
+load_dotenv()
 
 @app.route('/', defaults={'path': ''})
 @app.route('/<path:path>')
@@ -27,7 +29,19 @@ app.register_blueprint(geocoding_blueprint, url_prefix = '/geocoding')
 app.register_blueprint(profile_blueprint, url_prefix = '/profile')
 app.register_blueprint(place_image_blueprint, url_prefix = '/image')
 
-cred = credentials.Certificate('./firebase_auth.json')
+cred = credentials.Certificate({
+    "type": os.environ.get("FIREBASE_TYPE"),
+    "project_id": os.environ.get("FIREBASE_PROJECT_ID"),
+    "private_key_id": os.environ.get("FIREBASE_PRIVATE_KEY_ID"),
+    "private_key": os.environ.get("FIREBASE_PRIVATE_KEY"),
+    "client_email": os.environ.get("FIREBASE_CLIENT_EMAIL"),
+    "client_id": os.environ.get("FIREBASE_CLIENT_ID"),
+    "auth_uri": os.environ.get("FIREBASE_AUTH_URI"),
+    "token_uri": os.environ.get("FIREBASE_TOKEN_URI"),
+    "auth_provider_x509_cert_url": os.environ.get("FIREBASE_AUTH_PROVIDER_X509_CERT_URL"),
+    "client_x509_cert_url": os.environ.get("FIREBASE_CLIENT_X509_CERT_URL"),
+    "universe_domain": os.environ.get("FIREBASE_UNIVERSE_DOMAIN")
+})
 firebase_admin.initialize_app(cred)
 
 if __name__ == '__main__':
