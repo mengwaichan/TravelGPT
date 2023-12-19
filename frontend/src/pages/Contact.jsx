c1e1d6f9fdc77d61951b17623bd6659da253d78e
latiffetahaj <latiffetahaj@gmail.com>
2023-12-18 19:37:48 -0500

    added info in contact page

--- frontend/src/pages/Contact.jsx
+++ frontend/src/pages/Contact.jsx
@@ -12,61 +12,17 @@ const Contact = () => {
                     Got a technical issue? Want to send feedback about our product?
                     Let us know.
                 </p>
-                <form action="#" class="space-y-8">
-                    <div>
-                        <label
-                            for="email"
-                            class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"
-                        >
-                            Your email
-                        </label>
-                        <input
-                            type="email"
-                            id="email"
-                            class="text-sm w-700 px-4 py-2 border border-solid border-gray-300 rounded w-full bg-gray-50"
-                            placeholder="Email address"
-                            required
-                        />
-                    </div>
-                    <div>
-                        <label
-                            for="subject"
-                            class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"
-                        >
-                            Subject
-                        </label>
-                        <input
-                            type="text"
-                            id="subject"
-                            className="text-sm w-700 px-4 py-2 border border-solid border-gray-300 rounded w-full bg-gray-50"
-                            placeholder="Let us know how we can help you"
-                            required
-                        />
-                    </div>
-                    <div class="sm:col-span-2">
-                        <label
-                            for="message"
-                            class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-400"
-                        >
-                            Your message
-                        </label>
-                        <textarea
-                            id="message"
-                            rows="6"
-                            class="text-sm w-700 px-4 py-2 border border-solid border-gray-300 rounded w-full bg-gray-50"
-                            placeholder="Leave a comment..."
-                        ></textarea>
-                    </div>
-                    <div>
-
-                    </div>
-                    <button
-                        type="submit"
-                        class="mt-4 bg-blue-600 hover:bg-blue-700 px-4 py-2 text-white uppercase rounded text-xs tracking-wider"
+                <label
+                    for="email"
+                    class="block mb-2 text-sm  text-gray-900 dark:text-gray-300 text-center"
                     >
-                        Send message
-                    </button>
-                </form>
+                        Email us at:
+                </label>
+                <label
+                for="email"
+                class="block mb-2 text-lg font-medium text-gray-900 dark:text-gray-300 text-center">
+                travelgptfb@gmail.com
+                </label>
             </div>
         </section>
     );
