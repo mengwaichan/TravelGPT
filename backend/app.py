853112b4cbf4c34a75bceaf060d00d930384a78c
Mengwai Chan <44452206+mengwaichan@users.noreply.github.com>
2024-04-02 16:55:52 -0400

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -16,18 +16,7 @@ load_dotenv()
 app = Flask(__name__)
 CORS(app) 
 
-cred = credentials.Certificate({
-    "type": "service_account",
-    "project_id": os.getenv("FIREBASE_PROJECT_ID"),
-    "private_key_id": os.getenv("FIREBASE_PRIVATE_KEY_ID"),
-    "private_key": os.getenv("FIREBASE_PRIVATE_KEY").replace("\\n", "\n"),
-    "client_email": os.getenv("FIREBASE_CLIENT_EMAIL"),
-    "client_id": os.getenv("FIREBASE_CLIENT_ID"),
-    "auth_uri": os.getenv("FIREBASE_AUTH_URI"),
-    "token_uri": os.getenv("FIREBASE_TOKEN_URI"),
-    "auth_provider_x509_cert_url": os.getenv("FIREBASE_AUTH_PROVIDER_X509_CERT_URL"),
-    "client_x509_cert_url": os.getenv("FIREBASE_CLIENT_X509_CERT_URL")
-})
+cred = credentials.Certificate('./firebase_auth.json)
 
 firebase_admin.initialize_app(cred)
 
